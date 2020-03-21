c8d6162254a05791b461453371a493d8c4552fe0
trias <trias.2001@students.amikom.ac.id>
2020-03-21 13:52:18 +0700

    Praktikum OOP

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikumoop
+{
+    class Taxi
+    {
+        //properti
+        public string DriverName;
+        public Boolean OnDuty;
+        public int NumPassenger;
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty == true)
+            {Console.WriteLine("OnDuty : yes");}
+            else { Console.WriteLine("OnDuty : no"); }
+            Console.WriteLine("NumPassenger :{0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine(this.DriverName + " Sedang menjemput Penumpang");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine(this.DriverName + " Selesai mmengantar Penumpang");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikumoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "JONO";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
